337f02c3c75f285cb468e6fb7403ae20db011913
Mike <80673026+mssj-11@users.noreply.github.com>
2023-03-09 21:36:57 -0600

    REST API con Spring Boot

---
+++ src/main/java/com/bolsadeideas/springboot/secondwebservice/app/SpringBootSecondwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.secondwebservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecondwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecondwebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsadeideas/springboot/secondwebservice/app/SpringBootSecondwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.secondwebservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecondwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
